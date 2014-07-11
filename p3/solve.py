861d93e5904c5e910ad6e881a0e80f44d50bfa54
Huwenbo Shi <shihuwenbo@ucla.edu>
2014-07-11 12:20:07 -0700

    p1, p2, p3

--- p3/solve.py
+++ p3/solve.py
@@ -1,18 +1,15 @@
 import math
 
-num = 600851475143
-
 def find_largest_prime_factor(n):
     max_fac = -1
-    recurred = False
     for i in reversed(xrange(2, int(math.floor(math.sqrt(n))))):
         if(n%i == 0):
-            recurred = True
             curmax = find_largest_prime_factor(i)
             if(curmax > max_fac):
                 max_fac = curmax
-    if(recurred == False):
+    if(max_fac == -1):
         return n
     return max_fac
 
+num = 600851475143
 print find_largest_prime_factor(num)
